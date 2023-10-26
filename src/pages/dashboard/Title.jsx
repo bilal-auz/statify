f223fd04ce18e648b365698159ec47487c2b3643
Bilal Abouzid <imbilalabouzid@gmail.com>
2023-10-26 17:20:09 +0300

    Add logout button

--- src/pages/dashboard/Title.jsx
+++ src/pages/dashboard/Title.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { getProfileTile } from "../../services/DataFetchService";
+import { useHistory } from "react-router-dom";
 
 function Title() {
   useEffect(() => {
@@ -7,6 +8,7 @@ function Title() {
   }, []);
   const [displayName, setDisplayName] = useState("displayName");
   const [profilePic, setProfilePic] = useState("pfp");
+  const history = useHistory();
 
   const getProfileInfo = async () => {
     //getPofile info
@@ -15,6 +17,12 @@ function Title() {
     setDisplayName(data.display_name);
     setProfilePic(data.images[0].url);
   };
+
+  const handleLogout = () => {
+    localStorage.clear();
+    history.go(0);
+  };
+
   return (
     <React.Fragment>
       <div className="flex flex-row justtify-center items-center text-2xl font-[spotify-bold]">
@@ -23,9 +31,29 @@ function Title() {
       </div>
       <div className="flex flex-row justify-center items-center flex-wrap">
         <div className="mr-5 text-xl font-[spotify-txtBook]">{displayName}</div>
-        <div className="">
-          <img className="w-10 rounded-full" src={`${profilePic}`} alt="pfp" />
+        <div className="dropdown dropdown-bottom dropdown-end">
+          <label tabIndex={0}>
+            <img
+              className="w-10 rounded-full"
+              src={`${profilePic}`}
+              alt="pfp"
+            />
+          </label>
+          <ul
+            tabIndex={0}
+            className="dropdown-content z-[1] menu shadow bg-[#0c0c0c] rounded w-23 mt-2"
+          >
+            <li className="w-full">
+              <button
+                className="font-[spotify-bold] text-[#a83d3d] text-base p-0 rounded-none w-full"
+                onClick={handleLogout}
+              >
+                Logout
+              </button>
+            </li>
+          </ul>
         </div>
+        <div className=""></div>
       </div>
     </React.Fragment>
   );
